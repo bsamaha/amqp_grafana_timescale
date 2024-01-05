9f5837eddf6f766b8b82916bc1a18bd0ad76890b
bsamaha <59106056+bsamaha@users.noreply.github.com>
2024-01-05 11:55:22 -0600

    initial commit\

---
+++ amqp_consumer/amqp_consumer.py
@@ -0,0 +1,111 @@
+import time
+import pika
+import psycopg2
+import json
+
+# PostgreSQL connection parameters
+POSTGRES_HOST = "timescaledb"
+POSTGRES_DB = "postgres"
+POSTGRES_USER = "postgres"
+POSTGRES_PASSWORD = "spitfire"
+
+# RabbitMQ connection parameters
+RABBITMQ_HOST = "rabbitmq"
+RABBITMQ_QUEUE = "gnss_data_queue"
+RABBITMQ_USER = "guest"
+RABBITMQ_PASS = "guest"
+
+
+# Connect to PostgreSQL database
+def get_postgres_connection():
+    return psycopg2.connect(
+        host=POSTGRES_HOST,
+        database=POSTGRES_DB,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+    )
+
+
+def insert_into_db(json_data):
+    conn = get_postgres_connection()
+    cur = conn.cursor()
+
+    # Convert 'processed_time' to an integer
+    json_data["processed_time"] = int(float(json_data["processed_time"]))
+
+    try:
+        # Check if 'device_id' key exists in the JSON data
+        if "device_id" not in json_data:
+            print("Dropped message: 'device_id' not found in the message")
+            return
+
+        cur.execute(
+            """
+            INSERT INTO gngga (
+                time, full_time, lat, ns, lon, ew, quality,
+                num_sv, hdop, alt, alt_unit, sep, sep_unit,
+                diff_age, diff_station, processed_time, device_id
+            ) VALUES (
+                %(time)s, %(full_time)s, %(lat)s, %(ns)s, %(lon)s, %(ew)s, %(quality)s,
+                %(num_sv)s, %(hdop)s, %(alt)s, %(alt_unit)s, %(sep)s, %(sep_unit)s,
+                %(diff_age)s, %(diff_station)s, %(processed_time)s, %(device_id)s
+            )
+        """,
+            json_data,
+        )
+        conn.commit()
+    except Exception as e:
+        print(f"Database error: {e}")
+        conn.rollback()
+    finally:
+        cur.close()
+        conn.close()
+
+
+# Callback function when message is received
+def on_message_received(ch, method, properties, body):
+    json_data = json.loads(body)
+    print(f"Received data: {json_data}")
+
+    # Insert into PostgreSQL
+    insert_into_db(json_data)
+
+
+# Connect to RabbitMQ and start consuming
+def main():
+    credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
+    connection = None
+    max_retries = 5
+    retry_delay = 5  # seconds
+
+    for attempt in range(max_retries):
+        try:
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=RABBITMQ_HOST, credentials=credentials)
+            )
+            break  # Connection successful
+        except pika.exceptions.AMQPConnectionError as e:
+            if attempt < max_retries - 1:
+                print(
+                    f"Connection attempt {attempt + 1} of {max_retries} failed. Retrying in {retry_delay} seconds..."
+                )
+                time.sleep(retry_delay)
+            else:
+                print(
+                    "Max retries reached. Could not establish connection to RabbitMQ."
+                )
+                raise e
+
+    if connection is not None:
+        channel = connection.channel()
+        channel.queue_declare(queue=RABBITMQ_QUEUE, durable=True)
+        channel.basic_consume(
+            queue=RABBITMQ_QUEUE, on_message_callback=on_message_received, auto_ack=True
+        )
+
+        print(" [*] Waiting for messages. To exit press CTRL+C")
+        channel.start_consuming()
+
+
+if __name__ == "__main__":
+    main()
---
+++ timescaledb/init-db.sql
@@ -0,0 +1,47 @@
+-- init-db.sql
+
+CREATE TABLE devices (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    region TEXT,
+    registration_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE experiments (
+    id UUID PRIMARY KEY,
+    device_id TEXT NOT NULL REFERENCES devices(id),
+    start_time TIMESTAMP WITH TIME ZONE,
+    end_time TIMESTAMP WITH TIME ZONE,
+    test_description TEXT
+);
+
+CREATE TABLE gngga (
+    id SERIAL PRIMARY KEY,
+    time TIME,
+    full_time TIMESTAMP,
+    lat DOUBLE PRECISION,
+    ns TEXT,
+    lon DOUBLE PRECISION,
+    ew TEXT,
+    quality INTEGER,
+    num_sv INTEGER,
+    hdop DOUBLE PRECISION,
+    alt DOUBLE PRECISION,
+    alt_unit TEXT,
+    sep DOUBLE PRECISION,
+    sep_unit TEXT,
+    diff_age INTEGER,
+    diff_station TEXT,
+    processed_time BIGINT,
+    device_id TEXT NOT NULL REFERENCES devices(id),
+    -- change to not null later
+    experiment_id UUID REFERENCES experiments(id) 
+);
+
+-- Adding indexes
+CREATE INDEX idx_gngga_device_id ON gngga(device_id);
+CREATE INDEX idx_gngga_experiment_id ON gngga(experiment_id);
+
+-- insert initial test device with uuid for device and test-rpi for name
+INSERT INTO devices (id, name) VALUES ('test-rpi', 'test-rpi');
